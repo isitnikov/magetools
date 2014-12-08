7d78ab1f70e68f12c589ba22fc32abb63069d33e
Ihor Sytnykov <isitnikov@ebay.com>
2014-12-08 21:05:55 +0200

    Some changes in Module tree functionality. Added new features.

--- sources/Magetools/Module/Tree.php
+++ sources/Magetools/Module/Tree.php
@@ -2,15 +2,16 @@
 
 class Magetools_Module_Tree extends Magetools_Module_Abstract
 {
-    protected $_opts = 'ham:';
+    protected $_opts = 'hadm:';
 
     protected $_longOpts = array(
-        'help', 'all', 'module:'
+        'help', 'all', 'depends', 'module:'
     );
     protected $_optsMap = array(
-        'help'   => 'h',
-        'all'    => 'a',
-        'module' => 'm'
+        'help'    => 'h',
+        'all'     => 'a',
+        'module'  => 'm',
+        'depends' => 'd'
     );
 
     protected function _initModule()
@@ -29,6 +30,19 @@ class Magetools_Module_Tree extends Magetools_Module_Abstract
             }
         } elseif (isset($this->_moduleName)) {
             echo $this->_drawDependsTree($this->_moduleName, 0);
+
+            if ($this->_getOpt('depends')) {
+                $dependsOf = array();
+                $this->_findDependsFromModule($this->_moduleName, $dependsOf);
+
+                if (count($dependsOf)) {
+                    echo PHP_EOL . 'Modules which are depends from this module:';
+
+                    foreach ($dependsOf as $_moduleName => $_v) {
+                        echo $this->_drawDependsTree($_moduleName, 0);
+                    }
+                }
+            }
         } else {
             $this->_showHelp();
         }
@@ -37,10 +51,6 @@ class Magetools_Module_Tree extends Magetools_Module_Abstract
     protected function _drawDependsTree($moduleName, $level = 0)
     {
         if (!isset($this->_dependencies[$moduleName]) || !($moduleConfig = $this->_dependencies[$moduleName])) {
-            return '';
-        }
-
-        if (!isset($moduleConfig['depends']) || !count($moduleConfig['depends'])) {
             return '';
         }
 
@@ -55,6 +65,14 @@ class Magetools_Module_Tree extends Magetools_Module_Abstract
                 ? $this->_getColoredValue('true', 'green')
                 : $this->_getColoredValue('false', 'red')) . PHP_EOL;
             $str .= 'Dependencies: ' . PHP_EOL;
+            if (!isset($moduleConfig['depends']) || !count($moduleConfig['depends'])) {
+                $str .= PHP_EOL . '(Node <depends> is absent or empty.)' . PHP_EOL;
+                return $str;
+            }
+        }
+
+        if (!isset($moduleConfig['depends']) || !count($moduleConfig['depends'])) {
+            return '';
         }
 
         foreach ($moduleConfig['depends'] as $_moduleName) {
@@ -77,12 +95,13 @@ Magetools: Show module(-s) dependencies tree
 
 Usage:
     mage.phar --mt|--modtree -a | --all
-    mage.phar --mt|--modtree -m | --module app/etc/modules/Needed_Module.xml
+    mage.phar --mt|--modtree [-d]-m | [--depends]--module app/etc/modules/Needed_Module.xml
     mage.phar --mt|--modtree -h | --help
 
 Options:
-    -h --help   Show this screen
-    -a --all    Show all modules
+    -h --help    Show this screen
+    -a --all     Show all modules
+    -d --depends Show modules which are depends from this module
     -m --module Path to module declaration XML relative to Magento root folder
 
 USAGE;
